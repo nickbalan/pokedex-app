0ee06fbd8c154478a5be996d0927c41736e08554
nickbalan <nickolae.balan@gmail.com>
2021-03-03 09:43:37 +0100

    This commit will have created scripts.js file and will add a variable named favoriteFood
    This commit will have created a scripts.js file in a “js” subdirectory and added an alert and will add a variable named favoriteFood with a value. But also will have added some other variables with values.

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+
+//Creates an alert
+alert('Hello world');
+
+// Add some space between the variables
+let addSpace = '<br>';
+
+// Add two numbers together
+let simpleAddition = 2 + 2;
+document.write(simpleAddition + addSpace);
+
+// Assing a value to variable favoriteFood
+let favoriteFood = 'Sarmale';
+document.write(favoriteFood + addSpace);
+
+// Assign a value to an variable (myName)
+let myName = 'Bob\n';
+document.write(myName);
+
+// Assing a new value to the same variable (myName)
+myName = 'John Doe';
+document.write(myName + addSpace);
+
+// Multiply two numbers together
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize + addSpace);
+
+/* The result of doubling of the doubleSize variable and substracting half of the value of the original size variable*/
+let minSize = (doubleSize * 2) - (size * 2);
+document.write(minSize);
+
+
