30d524a4e26cb2906c0ea696236d6f81c31affd8
nickbalan <nickolae.balan@gmail.com>
2021-03-25 12:30:17 +0100

    This commit has deleted all code that is not relevant to the Pokemon project.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,3 @@
-
-//Creates an alert
-alert('Hello world');
-
 // Add some space between the variables
 let addSpace = '<br>';
 let add2Space = '<br><br>';
@@ -14,76 +10,6 @@ function addsSpace (){
 function adds2Space (){
 	document.write('<br><br>');
 };
-
-// Add two numbers together
-let simpleAddition = 2 + 2;
-document.write(simpleAddition + addSpace);
-
-// Assing a value to variable favoriteFood
-let favoriteFood = 'Sarmale';
-document.write(favoriteFood + addSpace);
-
-// Assign a value to an variable (myName)
-let myName = 'Bob\n';
-document.write(myName);
-
-// Assing a new value to the same variable (myName)
-myName = 'John Doe';
-document.write(myName + addSpace);
-
-// Multiply two numbers together
-let size = 100;
-let doubleSize = size * 2;
-document.write(doubleSize + addSpace);
-
-/* The result of doubling of the doubleSize variable and substracting half of the value of the original size variable*/
-let minSize = (doubleSize * 2) - (size * 2);
-document.write(minSize+addSpace);
-
-/* Displays a text with quotes*/
-let escapedtext1 = "This code displays \"quotes\"";
-document.write(escapedtext1+addSpace);
-
-/* Displays a text with backslash*/
-let escapedtext2 = 'This code displays \\backslash\\ and "quotes"';
-document.write(escapedtext2+addSpace);
-
-/* Displays a template literal form */
-let name = 'Nick';
-let age = 31;
-let message = `Hello!
-This is my first message here.
-My name is: ${name},
-and I'm ${age}.`;
-document.write(message+addSpace);
-
-/* Adds object car */
-let car = {
-	color: 'red',
-	mileage: 10
-};
-
-/* Adds object with another object nested inside it */
-let annesAge = 27;
-let anne = {
-	name: 'Anne',
-	age: annesAge,
-	child: {
-		name: 'Joe',
-		age:2
-	}
-};
-
-/* Adds Square Brackets with Objects */
-let currentUserName = 'sam';
-let userAges = {
-	anne: 27,
-	sam: 112,
-	megan: 97
-};
-
-userAges[currentUserName] = 113;
-
 
 /* Wraps the pokemonList array in IIFE. Adds a new variable to hold what IIFE will retun and assign the IIFE to that variable */
 let pokemonRepository = (function() {
@@ -161,83 +87,6 @@ for (let i = 0; i < pokemonListOut.length; i++) {
 	}
 };
 
-/* Adds some space between prints using function addsSpace */
+/* Adds some space between prints using function adds2Space */
 addsSpace();
-
-/* Adds my age with conditional if*/
-let myAge = 31;
-if (myAge === 30) {
-	document.write('You are 30 years old!'+addSpace);
-} else if (myAge === 31) {
-	document.write('You are 31 years old!'+addSpace);
-} else {
-	document.write('You are neither 30 nor 31 years old.'+addSpace);
-};
-
-/* Adds a loop that prints every number from 0 to 31*/
-for (let i = 0; i <= 31; i++) {
-	document.write(i+add2Space);
-};
-
-/* Adds some space between prints using function addsSpace */
-addsSpace();
-
-/* Adds a loop that prints an array of ages minus 2 years for each age */
-let ages = [20, 30, 21, 31, 35];
-for (let i = 0; i < ages.length; i++) {
-	document.write(ages[i] - 2 + addSpace)
-};
-
-/* Adds some space between prints using function addsSpace */
-addsSpace();
-
-/* Adds a loop that identifies and prints on DOM the maturity of people by ages (child, teenager, adult) */
-let person = [
-	{name: 'person1', age: 16},
-	{name: 'person2', age: 10},
-	{name: 'person3', age: 25}
-];
-
-for (let i = 0; i < person.length; i++) {
-	if (person[i].age < 19 && person[i].age > 13) {
-		document.write(person[i].name + ' is a teenager' + addSpace);
-	} else if (person[i].age < 13) {
-		document.write(person[i].name + ' is a child' + addSpace);
-	} else {
-		document.write(person[i].name + ' is an adult' + addSpace);
-	}
-};
-
-/* Adds some space between prints using function addsSpace */
-addsSpace();
-
-/* Adds a function expression that sum numbers */
-let add = function sum(x, y) {
-	return x + y;
-};
-
-let result1 = add(100, 200);
-document.write(result1);
-
-/* Adds some space between prints using function addsSpace */
-addsSpace();
-
-/* Adds a anonymous function that subtract numbers */
-let subtract = function (x, y) {
-	return x - y;
-};
-
-let result2 = subtract(200, 100);
-document.write(result2);
-
-/* Adds some space between prints using function addsSpace */
-addsSpace();
-
-/* Adds a callback function that multiplies a parameter with the results (result2) of the last subtraction*/
-function multiply(param) {
-	return param * result2; 
-};
-
-document.write(multiply(add(101, 99)));
-
 
