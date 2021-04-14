e30f1f425e4e84cc886992639bf28cc4b70b97ff
nickbalan <nickolae.balan@gmail.com>
2021-04-14 17:57:51 +0200

    This commit has fixed issue with showModal and deleted all code that is irrelevant with Pokemon project.

--- js/scripts.js
+++ js/scripts.js
@@ -10,14 +10,7 @@ let pokemonRepository = (function() {
 
 	/* This function adds new single item to the pokemonList array */
 	function add(pokemon) {
-		if (
-			typeof pokemon === 'object' && 
-			'name' in pokemon
-		) {
 			pokemonList.push(pokemon);
-		} else {
-			console.log('pokemon is not correct');
-		};
 	};
 
 	/* This function returns the pokemonList array */
@@ -33,17 +26,22 @@ let pokemonRepository = (function() {
 		/* Adds the names of the objects from pokemonList array on the buttons */
 		button.innerText = pokemon.name;
 		button.classList.add('button-class');
-		/* Adds an event listener to the button that calls showDetails function with pokemon parameter */
-		button.addEventListener('click', function() {
-			showDetails(pokemon)
-		});
 		/* Appends the button and the list to their parents */
 		listPokemon.appendChild(button);
 		pokemonList.appendChild(listPokemon);
+
+		detailsLister(button, pokemon);
+	};
+
+	/* Adds an event listener to the button that calls showDetails function with pokemon parameter */
+	function detailsLister(button, pokemon) {
+		button.addEventListener('click', function() {
+			showDetails(pokemon)
+		});
 	};
 
 	/* Fetches data from the API, then add each Pokémon in the fetched data to pokemonList */
-	function loadList(item) {
+	function loadList() {
 		return fetch(apiUrl).then(function(response) {
 			return response.json();
 			}).then(function(json){
@@ -61,56 +59,67 @@ let pokemonRepository = (function() {
 				});
 	};
 
-	/* Loads for each Pokemon (item) the detailed data using the detailsUrl property */
-	function loadDetails(item) {
-		let url = item.detailsUrl;
+	/* Loads for each Pokemon (item) the detailed data from the API */
+	function loadDetails(pokemon) {
+		let url = pokemon.detailsUrl;
 		return fetch(url).then(function(response) {
 			return response.json();
 		}).then(function(details) {
-			// adds the details to the item
-			item.imageUrl = details.sprites.front_default;
-			item.height = details.height;
-			item.types = details.types;
+			// adds the details to the items
+			pokemon.imageUrl = details.sprites.front_default;
+			pokemon.height = details.height;
+			pokemon.types = details.types;
 		}).catch(function(e) {
 			console.error(e);
 			});
 	};
 
 	/* Runs a console.log on the Pokemon objects to show details */
-	function showDetails(item) {
+	function showDetails(pokemon) {
 		/* Executes loadDetails function (that gets the Pokemon’s details from the server) in showDetails function (that is executed when a user clicks on a Pokemon button) */
-		pokemonRepository.loadDetails(item).then(function() {
-			console.log(item);
+		loadDetails(pokemon).then(function() {
+			showModal(pokemon);
 		});
 	};
 
 	/* Merges the modal container and its functions into pokemonRepository IIFE */
-	function showModal(title, text) {
+	function showModal(pokemon) {
 		/* Clears all existing modal content */
 		modalContainer.innerText = '';
-		/* Adds to modal container a div section and assigned a class to it */
+		
+		/* Adds to modal container a div section and assigned a classes to it */
 		let modal = document.createElement('div');
 		modal.classList.add('modal');
+		/* Selects the modal-container if the class is visible */
+		modalContainer.classList.add('is-visible');
+		
 		/* Adds to modal container a close button and assigned a class to it */
 		let closeButtonElement = document.createElement('button');
 		closeButtonElement.classList.add('modal-close');
 		/* Adds an inner text to the close button */
-	    closeButtonElement.innerText = "Close";
+	    closeButtonElement.innerText = 'Close';
 	    /* Adds an event listener to the close button, which activates the hideModal function */
 		closeButtonElement.addEventListener('click', hideModal);
-	    /* Adds an title tag */
-	    let titleElement = document.createElement('h1');
-	    titleElement.innerText = title;
-	    /* Adds a paragraph tag with some text */
-	    let contentElement = document.createElement('p');
-	    contentElement.innerText = text;
+	    
+	    /* Adds the name of Pokemon */
+	    let namePokemon = document.createElement('h1');
+	    namePokemon.innerText = pokemon.name;
+	    /* Adds the height of Pokemon */
+	    let heightPokemon = document.createElement('p');
+	    heightPokemon.innerText = `Height: ${pokemon.height}`;
+	    /* Adds the image of Pokemon */
+	    let imgPokemon = document.createElement('img');
+	    imgPokemon.src = pokemon.imageUrl;
+
+	    /* Adds an event listener to the close button, which activates the hideModal function */
+		closeButtonElement.addEventListener('click', hideModal);
 		/* Appends the elements to the modal */
 		modal.appendChild(closeButtonElement);
-		modal.appendChild(titleElement);
-		modal.appendChild(contentElement);
+		modal.appendChild(namePokemon);
+		modal.appendChild(heightPokemon);
+		modal.appendChild(imgPokemon);
 		modalContainer.appendChild(modal);
-		/* Selects the modal-container if the class is visible */
-		modalContainer.classList.add('is-visible');
+		
 	};
 
 	/* Saves the reject function of the promise in the dialogPromiseReject variable */
@@ -124,49 +133,7 @@ let pokemonRepository = (function() {
 		    dialogPromiseReject();
 		    dialogPromiseReject = null;
     	}
-  	}
-
-	/* Adds a showDialog function to create dialogs */
-	function showDialog(title, text) {
-		/* Calls the showModal to inherit its functions */
-		showModal(title, text);
-		/* Adds a confirm and cancel button to the modal */
-	  	let modal = modalContainer.querySelector('.modal');
-	  	/* Creates a 'Confirm' button, assignes it a class and adds an inner text into it */
-	  	let confirmButton = document.createElement('button');
-	  	confirmButton.classList.add('modal-confirm');
-	  	confirmButton.innerText = 'Confirm';
-	  	/* Creates a 'Cancel' button, assignes it a class and adds an inner text into it */
-		let cancelButton = document.createElement('button');
-		cancelButton.classList.add('modal-cancel');
-		cancelButton.innerText = 'Cancel';
-		/* Appends the new elements to this modal */
-	    modal.appendChild(confirmButton);
-	  	modal.appendChild(cancelButton);
-	  	/* Focuses the confirmButton so that the user can simply press Enter */
-	  	confirmButton.focus();
-	  	/* Returns a promise that resolves when confirmed, else rejects */
-	  	return new Promise((resolve, reject) => {
-	  		/* Rejects always the promise in the dialogPromiseReject no matter how the modal is closed */
-  			cancelButton.addEventListener('click', hideModal);
-  			confirmButton.addEventListener('click', () => {
-    			dialogPromiseReject = null; // Reset this
-    			hideModal();
-    			resolve();
-  				});
-  			/* This can be used to reject from other functions */
-  			dialogPromiseReject = reject;
-		});
-	};
-
-	/* Uses promise to check whether the user has confirmed or not */
-	document.querySelector('#show-dialog').addEventListener('click', () => {
-  		showDialog('Confirm action', 'Are you sure you want to do this?').then(function() {
-    	alert('confirmed!');
-  		}, () => {
-    	alert('not confirmed');
-  		});
-	});
+  	};
 
 	/* Sets the button to close if the Escape key is pressed */
 	window.addEventListener('keydown', (e) => {
@@ -186,12 +153,6 @@ let pokemonRepository = (function() {
 	    }
 	  });
 
-	/* Adds an event listener to the showModal function, which will activate the button */
-	document.querySelector('#show-modal').addEventListener('click', () => {
-		/* Adding 2 parameters that update the modal title and modal content */
-		showModal('Modal title', 'This is the modal content!');
-	});
-
 	/* Defines the keywords for the function that are used for execution outside of IIFE */
 	return {
 		add: add,
@@ -200,9 +161,8 @@ let pokemonRepository = (function() {
 		showDetails: showDetails,
 		loadList: loadList,
 		loadDetails: loadDetails,
-		showModal: showModal,
 		hideModal: hideModal,
-		showDialog: showDialog
+		showModal: showModal
 	};
 })();
 
